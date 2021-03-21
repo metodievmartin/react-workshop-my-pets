1e679050451646057474e8874ba8ea52bc1d7a47
Martin Metodiev <martin.metodiew@gmail.com>
2021-03-21 21:58:45 +0200

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+const Header = () => {
+    return (
+        <header id="site-header">
+            <nav className="navbar">
+
+                <section className="navbar-dashboard">
+                    <div className="first-bar">
+                        <a href="#">Dashboard</a>
+                        <a className="button" href="#">My Pets</a>
+                        <a className="button" href="#">Add Pet</a>
+                    </div>
+                    <div className="second-bar">
+                        <ul>
+                            <li>Welcome, guest!</li>
+                            <li><a href="#"><i className="fas fa-sign-out-alt"/> Logout</a></li>
+                        </ul>
+                    </div>
+                </section>
+                <section className="navbar-anonymous">
+                    <ul>
+                        <li><a href="#"><i className="fas fa-user-plus"/> Register</a></li>
+                        <li><a href="#"><i className="fas fa-sign-in-alt"/> Login</a></li>
+                    </ul>
+                </section>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
