808b88ea9b6e1c674d8568e7cd2ebb870007355a
Martin Metodiev <martin.metodiew@gmail.com>
2021-03-21 22:41:53 +0200

    Add styles to Header & Footer components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import './components/Footer/Footer.css';
+import './components/Header/Header.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="container">
-        <Header />
-        <Footer />
-    </div>
-  );
+    return (
+        <div className="container">
+            <Header/>
+
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
